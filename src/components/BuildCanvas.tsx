52934baf113335b37e1fae6d7aef29ccbf350d75
contatonoblelions-cpu <contatonoblelions@gmail.com>
2026-08-26 11:00:37 -0300

    modulo colocado usa object-fill (nao corta puxador ao mudar cm)

--- src/components/BuildCanvas.tsx
+++ src/components/BuildCanvas.tsx
@@ -382,7 +382,7 @@ function PlacedModuleBox({
           className="relative h-full w-full touch-none cursor-grab transition-transform active:cursor-grabbing"
         >
           {hasPhoto ? (
-                        <ModulePhoto name={m.moduleName} finish={finish} handle={handle} objectFit="cover" className="absolute inset-0 h-full w-full" />
+                        <ModulePhoto name={m.moduleName} finish={finish} handle={handle} objectFit="fill" className="absolute inset-0 h-full w-full" />
           ) : (
             <ModuleSchematic
               name={m.moduleName}
